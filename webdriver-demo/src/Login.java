958db6523cec62270c7515221f98920feb93ac8a
Jirapat Suwanjunee <suwanjunee987@gmail.com>
2022-02-22 21:23:52 +0700

    Login

---
+++ webdriver-demo/src/Login.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+    public static void main(String[] args) throws Exception {
+        System.setProperty("webdriver.chrome.driver","C://Users//Acer//Downloads//chromedriver_win32//chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://auspark.au.edu");
+
+        WebElement username = driver.findElement(By.id("Username"));
+        WebElement password = driver.findElement(By.id("Password"));
+        username.sendKeys("");
+        password.sendKeys("");
+        WebElement login = driver.findElement(By.className("btn--primary"));
+        login.click();
+    }
+}
